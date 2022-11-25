a8c50db6fe54e59e6ae0c1f63406cb0ed73ca051
Gabriel Pacheco <gabrielpacheco.wddigital@gmail.com>
2022-11-25 20:38:38 -0300

    feat: other configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyles from '../styles/globals'
+import theme from '../styles/theme'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>App Name</title>
+        <link rel="shorcut icon" href="/img/bg.png" />
+        <link rel="apple-touch-icon" href="/img/bg.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="description" content="App Name" />
+      </Head>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  )
 }
+
+export default App
